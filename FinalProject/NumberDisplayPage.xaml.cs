5ddc97c415e0633a6df8eae5edf0baed899d294c
caleb <calebchong4@icloud.com>
2024-12-06 20:36:59 -0500

    Made Question answers look nicer and display with correct widths and heights based on Question size

--- FinalProject/NumberDisplayPage.xaml.cs
+++ FinalProject/NumberDisplayPage.xaml.cs
@@ -17,6 +17,7 @@ namespace FinalProject
             var numberOptions = new List<Displayable> { new Number(val: 1, imageType: ImageType.Dice), new Number(val: 5, imageType: ImageType.Dice), new Number(val: 4, imageType: ImageType.Dice) };
             var question = new QuestionAndAnswers(numberOptions);
             question.Display(ParentLayout);
+            question.MauiSource.BackgroundColor = Color.FromRgb(100, 0, 0);
         }
     }
 }
--- FinalProject/Question.cs
+++ FinalProject/Question.cs
@@ -10,24 +10,40 @@ namespace FinalProject
         public Displayable Question { get; set; }
         public List<Displayable> OptionsList { get; set; }
         public HorizontalStackLayout OptionsLayout { get; set; }
-        public QuestionAndAnswers(List<Displayable> choices)
+        public int SpacingBetweenQuestions { get; set; }
+        public QuestionAndAnswers(List<Displayable> choices, int spacingBetweenQuestions = 30)
         {
             OptionsList = choices;
+            SpacingBetweenQuestions = spacingBetweenQuestions;
         }
         public override void Display(Layout parentLayout, int padding = 10, int spacing = 5, double imageHeight = 200, double imageWidth = 200, StackOrientation stackLayoutOrientation = StackOrientation.Vertical, LayoutOptions? horizontalOptions = null, LayoutOptions? verticalOptions = null, int textFontSize = 18, string? absoluteLayoutBounds = null, AbsoluteLayoutFlags absoluteLayoutFlags = AbsoluteLayoutFlags.None, bool addToParentLayout = true)
         {
             base.Display(parentLayout, padding, spacing, imageHeight, imageWidth, stackLayoutOrientation, horizontalOptions, verticalOptions, textFontSize, absoluteLayoutBounds, absoluteLayoutFlags, addToParentLayout);
             DisplayOptions(parentLayout);
         }
-        public void DisplayOptions(Layout parentLayout,int optionHeight = 100, int optionWidth = 100)
+        public void DisplayOptions(Layout parentLayout,double optionHeight = -1)
         {
+            if (optionHeight < 0)
+            {
+                optionHeight = (MauiSource.HeightRequest - MauiSource.Padding.VerticalThickness) / 3;
+            }
             OptionsLayout = new HorizontalStackLayout();
+            double totalOptionsWidth = MauiSource.WidthRequest - MauiSource.Padding.HorizontalThickness;
+            //- SpacingBetweenQuestions*OptionsList.Count
+            double optionWidth = (totalOptionsWidth - SpacingBetweenQuestions * (OptionsList.Count-1)) / OptionsList.Count;
+            OptionsLayout.Spacing = SpacingBetweenQuestions;
+            //OptionsLayout.Margin = MarginOfQuestions;
             foreach (var choice in OptionsList)
             {
                 choice.Display(parentLayout,imageHeight:optionHeight,imageWidth:optionWidth,addToParentLayout:false);
                 OptionsLayout.Add(choice.MauiSource);
             }
-            parentLayout.Add(OptionsLayout);
+            AbsoluteLayout intermediaryLayout = new AbsoluteLayout();
+            AbsoluteLayout.SetLayoutBounds(OptionsLayout, new Rect(0, optionHeight*2, totalOptionsWidth, optionHeight));
+            AbsoluteLayout.SetLayoutFlags(OptionsLayout, AbsoluteLayoutFlags.None);
+            intermediaryLayout.Children.Add(OptionsLayout);
+            MauiSource.Add(intermediaryLayout);
+            
         }
 
     }
