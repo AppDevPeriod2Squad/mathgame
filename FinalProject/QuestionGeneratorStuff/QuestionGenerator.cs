a2fe90ec9cd11b20ac1a39e7e7df473bf124ffdd
caleb <calebchong4@icloud.com>
2024-12-08 00:18:02 -0500

    Started work on a way to generate questions randomly from a set of restrictions. WIP.

---
+++ FinalProject/QuestionGeneratorStuff/QuestionGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject.QuestionGeneratorStuff
+{
+    public class QuestionGenerator
+    {
+        private List<ImageType>? PotentialAnswerTypes { get; set; }
+        private List<Number> Answers { get; set; }
+        private Displayable QuestionPrompt { get; set; }
+        public QuestionGenerator(int numOfAnswers = 4, List<ImageType>? potentialAnswerTypes = null, QuestionSuperType superType = QuestionSuperType.None, QuestionSubType subType = QuestionSubType.None)
+        {
+
+        }
+        private void GeneratePrompt(QuestionSubType subType, QuestionSuperType superType)
+        {
+            int numOfNumbersInQuestion = 0;
+            switch (subType)
+            {
+                case QuestionSubType.ToTen:
+                    numOfNumbersInQuestion = 2;
+                    break;
+            }
+
+        }
+    }
+}
---
+++ FinalProject/QuestionGeneratorStuff/QuestionGeneratorNumberRange.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject.QuestionGeneratorStuff
+{
+    public class QuestionGeneratorNumberRange
+    {
+        // Mins and Maxes are INCLUSIVE of themselves
+        public double Min { get; set; }
+        public double Max { get; set; }
+        public double Interval { get; set; } // is the amnt of steps inbetween each possible number
+        // for example if you want every int from 1-9 the Interval would be 1
+        public QuestionGeneratorNumberRange(double min, double max, double interval = 1)
+        {
+            Min = min;
+            Max = max;
+            Interval = interval;
+        }
+    }
+}
