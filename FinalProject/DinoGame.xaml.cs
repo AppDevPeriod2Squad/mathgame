220f17ca39170665478518a288788c995fedd546
kiran049 <98996914+kiran049@users.noreply.github.com>
2024-12-20 09:03:13 -0500

    dino update

--- FinalProject/DinoGame.xaml.cs
+++ FinalProject/DinoGame.xaml.cs
@@ -22,44 +22,28 @@ public partial class DinoGame : GamePage
 
         DinoImage.GestureRecognizers.Add(panGesture);
     }
-	public void QuestionClicked(object sender, EventArgs e)
-	{
-		question = generator.Generate(QuestionSuperType.FindGreatest);
-		question.Display(mainLayout, new DisplayableArgs(absoluteLayoutBounds: "0,0,1000,1000"));
-		if (e is QuestionEventArgs args)
-		{
-			if (args.WasCorrect)
-			{
-				mainLayout.Add(new Label() { Text = "You were correct !" });
-			}
-			else
+
+    private void OnPanUpdated(object sender, PanUpdatedEventArgs e)
+    {
+        if (sender is Image dino)
+        {
+            switch (e.StatusType)
             {
-                mainLayout.Add(new Label() { Text = "You were wrong :(" });
+                case GestureStatus.Started:
+                    initialX = dino.TranslationX;
+                    initialY = dino.TranslationY;
+                    break;
+
+                case GestureStatus.Running:
+                    dino.TranslationX = initialX + e.TotalX;
+                    dino.TranslationY = initialY + e.TotalY;
+                    break;
+
+                case GestureStatus.Completed:
+                    initialX = dino.TranslationX;
+                    initialY = dino.TranslationY;
+                    break;
             }
         }
     }
-    
-    //   public void QuestionSetup()
-    //{
-    //	generator = new QuestionGenerator(new EventHandler((sender, e) => QuestionClicked(sender, e)), potentialAnswerTypes: new List<ImageType>() { ImageType.Dice, ImageType.Food });
-    //	// probably move this stuff into generator constructor
-    //	question = generator.Generate(QuestionSuperType.FindGreatest);
-    //       question.Display(mainLayout, new DisplayableArgs(absoluteLayoutBounds: "0,0,1000,1000"));
-    //   }
-    //public void QuestionClicked(object sender, EventArgs e)
-    //{
-    //	question = generator.Generate(QuestionSuperType.FindGreatest);
-    //	question.Display(mainLayout, new DisplayableArgs(absoluteLayoutBounds: "0,0,1000,1000"));
-    //	if (e is QuestionEventArgs args)
-    //	{
-    //		if (args.WasCorrect)
-    //		{
-    //			mainLayout.Add(new Label() { Text = "You were correct !" });
-    //		}
-    //		else
-    //           {
-    //               mainLayout.Add(new Label() { Text = "You were wrong :(" });
-    //           }
-    //       }
-    //}
 }
