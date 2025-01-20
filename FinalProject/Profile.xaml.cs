ff2088aee62944c9407641998c9e334bcfef008b
caleb <calebchong4@icloud.com>
2025-01-20 16:32:20 -0500

    :(

--- FinalProject/Profile.xaml.cs
+++ FinalProject/Profile.xaml.cs
@@ -1,35 +1,25 @@
 using FinalProject.Gacha;
+using System.ComponentModel;
 
 namespace FinalProject;
 
 public partial class Profile : ContentPage
 {
-	private User user;
-
-
-	private Database db;
+    private User user;
+    private Database db;
     private IDispatcherTimer _timer;
 
     public Profile(Database db)
-	{
-        InitializeComponent();
-        user = new User();
-        user.Name = "Sigma Male";
-        //user.Picture = "https://www.newtraderu.com/wp-content/uploads/9-Secret-Strengths-Of-The-Sigma-MaleUnderstanding-The-Lone-Wolf-scaled.jpg";
-        user.XP = 69;
-        user.GamesCompleted = 42;
-        user.Quarters = 2;
-        user.Dimes = 3;
-        user.Nickels = 4;
-        user.Pennies = 5;
+    {
         this.db = db;
         _timer = Dispatcher.CreateTimer();
         _timer.Interval = TimeSpan.FromSeconds(1); // Set interval
         _timer.Tick += Timer_Tick; // Attach the event
         _timer.Start(); // Start the timer
-        Setup();
-		container.Add(new NavElement(container, db));
 
+        InitializeComponent();
+        Setup();
+        container.Add(new NavElement(container, db));
     }
 
     private void Timer_Tick(object sender, EventArgs e)
@@ -39,7 +29,7 @@ public partial class Profile : ContentPage
     }
 
     public async void Setup()
-	{
+    {
         user = await db.GetUserAsync();
         profileName.Text = user.Name;
         profilePicture.Source = Translator.pfpLinks[user.Picture];
